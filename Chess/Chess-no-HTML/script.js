8105bb51efb060957d4e480b91ea972a6ada1461
yuvron <yuvr99@gmail.com>
2022-04-17 11:17:21 +0300

    using table insert

--- Chess/Chess-no-HTML/script.js
+++ Chess/Chess-no-HTML/script.js
@@ -3,38 +3,29 @@ const BOARD_SIZE = GAME_SIZE + 2;
 
 //Creates a table element (the board) and appending it all the rows and cells of the table
 function createBoard() {
-	const body = document.getElementsByTagName("body")[0];
+	const body = document.body;
 	const board = document.createElement("table");
+	board.id = "board";
 	body.appendChild(board);
-	board.setAttribute("id", "board");
+	let isSideRow = false;
+	let isSideColumn = false;
 	//loop to create 10 rows (8 for the game, 2 for the letters)
 	for (let i = BOARD_SIZE - 1; i >= 0; i--) {
-		const row = document.createElement("tr"); //Row number i
-		//Checking if the current row is a game row or a letters row
-		if (i == 0 || i == BOARD_SIZE - 1) {
-			for (let j = 0; j < BOARD_SIZE; j++) {
-				const cell = document.createElement("th");
-				row.appendChild(cell);
-				if (j == 0 || j == BOARD_SIZE - 1) {
-					cell.classList.add("letter");
+		const row = board.insertRow();
+		isSideRow = i == 0 || i == BOARD_SIZE - 1;
+		for (let j = 0; j < BOARD_SIZE; j++) {
+			const cell = row.insertCell();
+			cell.id = "cell-" + i + "-" + j;
+			isSideColumn = j == 0 || j == BOARD_SIZE - 1;
+			if (isSideRow) {
+				cell.classList.add("letter");
+				if (isSideColumn) cell.classList.add("number");
+				else cell.innerText = String.fromCharCode(j + 64);
+			} else {
+				if (isSideColumn) {
 					cell.classList.add("number");
-				} else {
-					cell.className = "letter";
-					cell.innerText = String.fromCharCode(j + 64);
-				}
-			}
-		} else {
-			//Handling the actual game rows
-			for (let j = 0; j < BOARD_SIZE; j++) {
-				let cell;
-				//Side cells hold numbers, they are not games cells
-				if (j == 0 || j == BOARD_SIZE - 1) {
-					cell = document.createElement("th");
-					cell.className = "number";
 					cell.innerText = i;
 				} else {
-					//Handling the actual game cells
-					cell = document.createElement("td");
 					cell.classList.add("tile");
 					if (i % 2 == 0) {
 						if (j % 2 == 0) cell.classList.add("black");
@@ -44,10 +35,8 @@ function createBoard() {
 						else cell.classList.add("black");
 					}
 				}
-				row.appendChild(cell);
 			}
 		}
-		board.appendChild(row);
 	}
 }
 
